5a721f198d8c80e58c5176b468af7db408d39637
helens <168898018+HelenShek@users.noreply.github.com>
2024-09-01 14:58:02 +0800

    通讯录按年龄大小排序.c

--- 通讯录按年龄大小排序.c
+++ 通讯录按年龄大小排序.c
@@ -1,20 +1,20 @@
 /*
-����Ա��ʯ��Դ
-���ʱ�䣺2024.9.1
+程序员：Helen Shek
+编程时间：2024.9.1
 */
 
 /*
-ͨѶ¼���� 
-����n�����ѵ���Ϣ���������������ա��绰���룬����Ҫ���д����
-��������Ӵ�С��˳���������ͨѶ¼����Ŀ��֤�����˵����վ�����ͬ��
+通讯录排序 
+输入n个朋友的信息，包括姓名、生日、电话号码，本题要求编写程序，
+按照年龄从大到小的顺序依次输出通讯录。题目保证所有人的生日均不相同。
 
-�����ʽ:
-�����һ�и���������n��<10�������n�У�ÿ�а��ա����� ���� �绰���롱�ĸ�ʽ
-����һλ���ѵ���Ϣ�����С��������ǳ��Ȳ�����10��Ӣ����ĸ��ɵ��ַ�����
-�����ա���yyyymmdd��ʽ�����ڣ����绰���롱�ǲ�����17λ�����ּ�+��-��ɵ��ַ�����
+输入格式:
+输入第一行给出正整数n（<10）。随后n行，每行按照“姓名 生日 电话号码”的格式
+给出一位朋友的信息，其中“姓名”是长度不超过10的英文字母组成的字符串，
+“生日”是yyyymmdd格式的日期，“电话号码”是不超过17位的数字及+、-组成的字符串。
 
-�����ʽ:
-��������Ӵ�С������ѵ���Ϣ����ʽͬ�����
+输出格式:
+按照年龄从大到小输出朋友的信息，格式同输出。
 */
 
 #include <stdio.h> 
@@ -37,32 +37,32 @@ int main  ()
 	}
 	
 	struct Contact temp,*p,*q,*m[n];  
-	//ʹ��ָ��Խṹ�������˳����н���������m���ڴ�ŵ�ַ 
-	for (i = 0;i < n;i++)  //ð������ 
+	//使用指针对结构体变量的顺序进行交换，数组m用于存放地址 
+	for (i = 0;i < n;i++)  //冒泡排序 
 	{
 		for (j = i;j < n;j++)
 		{
-			p = &friends[i];  //ָ�����p��q�ֱ�ָ��ṹ�����friends[i]��friends[j] 
+			p = &friends[i];  //指针变量p、q分别指向结构体变量friends[i]、friends[j] 
 			q = &friends[j];
-			if (friends[i].birthday > friends[j].birthday )  //friends[j]������� 
-			{  //��ָ�������ָ�ĵĽṹ��������л��� 
+			if (friends[i].birthday > friends[j].birthday )  //friends[j]年龄更大 
+			{  //将指针变量所指的的结构体变量进行互换 
 				temp = *p;
 				*p = *q;
 				*q = temp;
 			}
 		}
-		m[i] = p;  //ÿһ��ð����������󶼰����������������ڵĽṹ������ĵ�ַ��������m�� 
-	}  //��m[0]��ŵ��������������ѵ���Ϣ��m[1]��ŵ��ǵڶ�������ѵ���Ϣ 
+		m[i] = p;  //每一次冒泡排序结束后都把年龄最大的朋友所在的结构体变量的地址存入数组m中 
+	}  //即m[0]存放的是年龄最大的朋友的信息，m[1]存放的是第二大的朋友的信息 
 	
 	for (i = 0;i < n;i++)
-	{  //��˳���������m�д�ŵ�ָ������Ӧ�Ľṹ����������� 
+	{  //按顺序输出数组m中存放的指针所对应的结构体变量的内容 
 		printf ("%s ", m[i]->name);
 		printf ("%d ", m[i]->birthday);
 		if (i != n-1)
 		{
 			printf ("%s\n", m[i]->number); 
 		}
-		else printf ("%s", m[i]->number);  //ĩβ���ַ�������Ҫ����
+		else printf ("%s", m[i]->number);  //末尾的字符串不需要换行
 	}
 	return 0;
 }
